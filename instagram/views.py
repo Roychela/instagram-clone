757db9018de78ac895767dcf2f35bb902e25a1be
Roy Chela <roychela@gmail.com>
2019-07-28 15:50:34 +0300

    Add dummy view function to test model functionality

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Image
 # Create your views here.
+
+
+
+
+def home(request):
+    context = {
+        'images': Image.objects.all()
+    }
+    return render(request, 'home.html', context)
