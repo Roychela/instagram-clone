c9c1435651d2c16c9e63c8a0883c538a814e936f
Roy Chela <roychela@gmail.com>
2019-07-28 13:39:07 +0300

    Add Image model

--- instagram/models.py
+++ instagram/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Image(models.Model):
+    image_name = models.CharField(max_length=100)
+    image_caption = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = 'images/', blank = True)
+
+    def __str__(self):
+        return self.title
