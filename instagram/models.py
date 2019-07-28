b83cbf181cc3222964233bdaa3cac13abdf3f98f
Roy Chela <roychela@gmail.com>
2019-07-28 18:59:56 +0300

    Add create, save and search profile functions

--- instagram/models.py
+++ instagram/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 
 # Create your models here.
 class Image(models.Model):
@@ -19,6 +22,20 @@ class Image(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    @classmethod
+    def search_profile(cls, name):
+        profile = Profile.objects.filter(user__username__icontains = name)
+        return profile
 
     def __str__(self):
         return f'{self.user.username} Profile'
